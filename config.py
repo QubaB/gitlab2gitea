beb5ff5426887748b526d1f44236c1b2fe715215
QubaB <74395874+QubaB@users.noreply.github.com>
2025-07-25 07:06:27 +0200

    Create config.py
    configuration file

---
+++ config.py
@@ -0,0 +1,16 @@
+# === CONFIG ===
+GITLAB_URL = "https://mygitlab.com"
+GITLAB_TOKEN = "token_generated_on_Gitlab_Web"         # must be crated in web ui
+GITLAB_PROJECT = "username/project"
+
+GITEA_URL = "https://gitea.com"
+GITEA_TOKEN = "token_generated_on_Gitea_Web"   # must be crated in web ui
+GITEA_OWNER = "username"
+GITEA_REPO = "project"
+
+MYSQL_HOST="127.0.0.1" # hostname of gitea MYSQL server, here the same host
+MYSQL_PORT=3306                 # gitea database server port
+MYSQL_USER="gitea"              # gitea database user name
+MYSQL_PASSWORD="pasword_to_database"     # gitea database user password
+MYSQL_DATABASE="gitea"  # gitea database name 
+# === CONFIG END ===
